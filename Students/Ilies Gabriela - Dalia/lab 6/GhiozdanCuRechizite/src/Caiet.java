749094f000ac93691df8b98ac404e148182399ec
iliesgabi <iliesgabi99@yahoo.ro>
2019-11-07 10:57:15 +0200

    lab6

--- Students/Ilies Gabriela - Dalia/lab 6/GhiozdanCuRechizite/src/Caiet.java
+++ Students/Ilies Gabriela - Dalia/lab 6/GhiozdanCuRechizite/src/Caiet.java
@@ -1,13 +1,15 @@
 
-public class Caiet extends Rechizita {
-	public Caiet(String eticheta) {
-		this.eticheta = eticheta;
+public class Caiet extends Rechizita{
+	public Caiet(String eticheta)
+	{
+		this.eticheta=eticheta;
 	}
-
-	public String getNume() {
-		String caiet = "Caiet ";
-		caiet = caiet.concat(this.eticheta);
+	
+	public String getNume()
+	{
+		String caiet="Caiet ";
+		caiet=caiet.concat(this.eticheta);
 		return caiet;
-
+		
 	}
 }
